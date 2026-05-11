d8cf6eac27fb41e8ef4c9bcdb03cdc0f5e0bf02d
Hernan Ramirez <h.ramirezr@uniandes.edu.co>
2026-05-11 16:22:07 -0500

    add dependencies

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot(
+      'mongodb://root:secret@localhost:27017/travel_planner?authSource=admin',
+    ),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
