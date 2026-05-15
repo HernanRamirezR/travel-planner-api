c8a74184f2825d8620288e53b4f08ff898086f25
Hernan Ramirez <h.ramirezr@uniandes.edu.co>
2026-05-15 18:02:05 -0500

    User small API

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
-
+import { Body, Controller, Post, Delete, Param} from '@nestjs/common';
+import { UsersService } from './users.service';
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+
+    constructor(private readonly usersService: UsersService){}
+
+    @Post()
+    create( @Body() body: { name: string; email: string}) {
+        return this.usersService.create(body);
+    }
+
+    @Delete(':id')
+    remove( @Param('id') id: string) {
+
+        return this.usersService.remove(id);
+    }
+
+}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { User, UserDocument } from './schemas/users.schema';
+import { Model } from 'mongoose';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+    
+    constructor( @InjectModel(User.name) private readonly userModel: Model<UserDocument>) {}
+
+    async create(body: {name: string; email: string}) {
+
+        return this.userModel.create(body);
+    }
+
+    async remove(id: string) {
+        return this.userModel.findByIdAndDelete(id);
+    }
+}
