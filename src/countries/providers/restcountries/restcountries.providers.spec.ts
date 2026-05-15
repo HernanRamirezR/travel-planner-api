ae3dfd94480fdac40a72227dc7721ab7d3884f33
Hernan Ramirez <h.ramirezr@uniandes.edu.co>
2026-05-14 21:39:51 -0500

    ignore

--- src/countries/providers/restcountries/restcountries.providers.spec.ts
+++ src/countries/providers/restcountries/restcountries.providers.spec.ts
@@ -1,15 +1,15 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { Restcountries } from './restcountries.provider';
+import { RestCountriesProvider } from './restcountries.provider';
 
 describe('Restcountries', () => {
-  let provider: Restcountries;
+  let provider: RestCountriesProvider;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [Restcountries],
+      providers: [RestCountriesProvider],
     }).compile();
 
-    provider = module.get<Restcountries>(Restcountries);
+    provider = module.get<RestCountriesProvider>(RestCountriesProvider);
   });
 
   it('should be defined', () => {
